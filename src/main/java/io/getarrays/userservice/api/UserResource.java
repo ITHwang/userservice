cd720e8b78d74723f2b50d5b28aebe5b6b07cb2d
ITHwang <intaek428@gmail.com>
2022-01-25 14:26:05 +0900

    access/refresh token을 response body에 담아서 주기(~1:27:08)

--- src/main/java/io/getarrays/userservice/api/UserResource.java
+++ src/main/java/io/getarrays/userservice/api/UserResource.java
@@ -6,7 +6,6 @@ import io.getarrays.userservice.service.UserService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
